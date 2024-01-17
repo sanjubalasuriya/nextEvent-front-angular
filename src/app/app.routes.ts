b085a86549a5779f3c92578ffe73ab28a22a8eb5
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-17 22:10:39 +0530

    update app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home/home-page/home-page.component";
+import {EventViewPageComponent} from "./pages/event/event-view-page/event-view-page.component";
+import {NotFoundPageComponent} from "./core/not-found-page/not-found-page.component";
+import {EventPageComponent} from "./pages/event/event-page/event-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'/home',pathMatch:'full'},
+    {path:'home', component:HomePageComponent},
+    {path:'events', component:EventPageComponent},
+    {path:'event-details/:id',component:EventViewPageComponent},
+    {path:'**',component:NotFoundPageComponent}
+];
