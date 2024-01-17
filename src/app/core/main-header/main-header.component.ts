e6e9fa2020047fedb5ba1f3defe26775f78ae34c
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-17 22:03:11 +0530

    Create header

---
+++ src/app/core/main-header/main-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-header.component.html',
+  styleUrl: './main-header.component.scss'
+})
+export class MainHeaderComponent {
+
+}
