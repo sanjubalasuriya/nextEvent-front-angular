86084bea48e4a689d8fbee544890547a08252fa3
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-20 22:55:35 +0530

    update approutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {HomePageComponent} from "./pages/home/home-page/home-page.component";
 import {EventViewPageComponent} from "./pages/event/event-view-page/event-view-page.component";
 import {NotFoundPageComponent} from "./core/not-found-page/not-found-page.component";
 import {EventPageComponent} from "./pages/event/event-page/event-page.component";
+import {BookTicketComponent} from "./pages/event/book-ticket/book-ticket.component";
 
 export const routes: Routes = [
     {path:'', redirectTo:'/home',pathMatch:'full'},
     {path:'home', component:HomePageComponent},
     {path:'events', component:EventPageComponent},
     {path:'event-details/:id',component:EventViewPageComponent},
+    {path:'book-tickets/:id',component:BookTicketComponent},
     {path:'**',component:NotFoundPageComponent}
 ];
