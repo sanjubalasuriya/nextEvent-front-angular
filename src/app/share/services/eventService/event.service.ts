19b45e1fc2c934ad44aca3bfe3ee8dab3297ebb0
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-20 22:56:30 +0530

    update event service

---
+++ src/app/share/services/eventService/event.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {Observable, Subject} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  constructor(private http:HttpClient) { }
+
+  private _refreshNeeded$ = new Subject<void>();
+
+  public getEvents():Observable<any>{
+    return this.http.get('http://localhost:8003/api/v1/event/all')
+  }
+
+  public getEventById(id:any):Observable<any>{
+    return this.http.get('http://localhost:8003/api/v1/event/get-by-id?eventId='+id)
+  }
+
+  public getCategoryByEventId(id:any):Observable<any>{
+    return this.http.get('http://localhost:8003/api/v1/ticket-category/get-by-eventId?eventId='+id)
+  }
+
+  get refreshList(){
+    return this._refreshNeeded$;
+  }
+}
