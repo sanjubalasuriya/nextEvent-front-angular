b66b053a4bc9def4489414a3470cafc5f12e619a
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-27 20:22:51 +0530

    create main-slider.component.html

---
+++ src/app/pages/home/home-page/inner-items/main-slider/main-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-slider.component.html',
+  styleUrl: './main-slider.component.scss'
+})
+export class MainSliderComponent {
+
+}
