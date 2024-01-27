fe81b67220855476d3efea83c2d8579b18399e0c
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-27 20:19:57 +0530

    create reset-password-page.component

---
+++ src/app/pages/security/reset-password-page/reset-password-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './reset-password-page.component.html',
+  styleUrl: './reset-password-page.component.scss'
+})
+export class ResetPasswordPageComponent {
+
+}
