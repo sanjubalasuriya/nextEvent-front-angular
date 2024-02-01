61ffd5cd1d4cf6353b3b49f9f79740639756d2aa
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-02-01 20:00:17 +0530

    update sign-up component

--- src/app/pages/security/signup-page/signup-page.component.ts
+++ src/app/pages/security/signup-page/signup-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.scss'
 })
