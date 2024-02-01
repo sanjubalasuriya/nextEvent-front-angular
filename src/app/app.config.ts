c40442b7fbf024ccf675ddcfe7ba2b579ec1bb72
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-02-01 19:59:01 +0530

    update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {httpManagerInterceptor} from "./share/interceptors/http-manager.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideAnimations()]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimations(),
+    provideHttpClient(withInterceptors([httpManagerInterceptor]))
+  ]
 };
