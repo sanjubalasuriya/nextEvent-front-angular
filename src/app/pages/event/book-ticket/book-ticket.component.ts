fef97d3f3b47ea5b015177c19d11c09ba4f5fc2f
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-02-01 20:00:45 +0530

    update book-ticket component

--- src/app/pages/event/book-ticket/book-ticket.component.ts
+++ src/app/pages/event/book-ticket/book-ticket.component.ts
@@ -1,10 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, Injectable, OnInit, PLATFORM_ID} from '@angular/core';
 import {ActivatedRoute, RouterLink} from "@angular/router";
 
 import {EventService} from "../../../share/services/eventService/event.service";
-import {DecimalPipe, NgForOf} from "@angular/common";
+import {DecimalPipe, NgForOf, NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {isPlatformBrowser} from '@angular/common';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatIconModule} from "@angular/material/icon";
+import {CartService} from "../../../share/services/cartService/cart.service";
+import {response} from "express";
 
+@Injectable()
 @Component({
     selector: 'app-book-ticket',
     standalone: true,
@@ -12,91 +19,105 @@ import {FormsModule} from "@angular/forms";
         RouterLink,
         NgForOf,
         FormsModule,
-        DecimalPipe
+        DecimalPipe,
+        MatInputModule,
+        MatSelectModule,
+        MatIconModule,
+        NgIf
     ],
     templateUrl: './book-ticket.component.html',
     styleUrl: './book-ticket.component.scss'
 })
 export class BookTicketComponent implements OnInit {
 
-    events: Array<any> = [];
+    category: Array<any> = [];
 
 
+    constructor(private eventService: EventService, private activeRoute: ActivatedRoute, private cartService: CartService) {
 
-    selected = [
-        {id: 1},
-        {id: 2},
-        {id: 3},
-        {id: 4},
-        {id: 5},
-        {id: 6},
-        {id: 7},
-        {id: 8},
-        {id: 9},
-        {id: 10}
-
-    ]
-    constructor(private eventService: EventService, private activeRoute: ActivatedRoute) {
     }
 
     ngOnInit(): void {
-        localStorage.removeItem('ticket');
+        this.cartService.refreshList.subscribe(() => {
+            this.allTickets()
+        })
+        this.allTickets()
 
+    }
+
+    allTickets() {
         this.activeRoute.queryParams.subscribe(params => {
             this.eventService.getCategoryByEventId(
                 params['id']
             ).subscribe(response => {
-                this.events = response.data
+                this.category = response.data
+
+                this.cartService.getByCustomerId(1).subscribe(response => {
+                    console.log(this.category)
+                    if (response.data.length > 0) {
+
+                        for (let i = 0; i < this.category.length; i++) {
+                            for (let j = 0; j < response.data.length; j++) {
+                                if (this.category[i].categoryId === response.data[j].ticketCategory) {
+                                    this.category[i].qty = response.data[j].qty;
+                                    this.category[i].cartId = response.data[j].cartId;
+
+                                }
+                            }
+                        }
+                    }
+                })
+            })
+        });
+    }
+
+
+    addTicket(data: any) {
+        this.cartService.addTicket(
+            data.categoryId
+        ).subscribe(response => {
+            console.log(response);
+        })
+    }
+
+    removeTicket(data: any) {
+    }
+
+    qty = 0;
+
+    plusTicketCount(data: any) {
+
+        this.qty = data.qty + 1
+        if (this.qty < 11) {
+            this.cartService.updateTicket(
+                data.cartId,
+                data.categoryId,
+                this.qty
+            ).subscribe(response => {
                 console.log(response)
             })
-
-        });
-
-        this.loadTotal();
-
-    }
-
-    select:Number=0;
-
-
-    ticketList:any = [];
-    onSelect(data: any){
-        let ticketNull = localStorage.getItem('ticket');
-        if(ticketNull == null){
-            let storeDataGet:any = [];
-            storeDataGet.push(data);
-            localStorage.setItem('ticket',JSON.stringify(storeDataGet));
-        }else {
-            var id = data.categoryId;
-            let index:number = -1;
-            this.ticketList = JSON.parse(localStorage.getItem('ticket')!);
-            for(let i=0;i < this.ticketList.length; i++){
-                if(parseInt(id) === parseInt(this.ticketList[i].categoryId)){
-                    this.ticketList[i].select = data.select;
-                    index = i;
-                    break;
-                }
-            }
-            if(index == -1){
-                this.ticketList.push(data);
-                localStorage.setItem('ticket',JSON.stringify(this.ticketList));
-            }else{
-                localStorage.setItem('ticket',JSON.stringify(this.ticketList));
-
-            }
         }
-        this.loadTotal();
+
     }
 
-    total:number = 0;
-    getTickets:any =[];
-    loadTotal(){
-        if(localStorage.getItem('ticket')){
-            this.getTickets = JSON.parse(localStorage.getItem('ticket')!);
-            this.total = this.getTickets.reduce(function (acc:any, val:any){
-                return acc + (val.select * val.ticketPrice);
-            },0)
+    minusTicketCount(data: any) {
+        this.qty = data.qty - 1
+        if (this.qty != 0) {
+            this.cartService.updateTicket(
+                data.cartId,
+                data.categoryId,
+                this.qty
+            ).subscribe(response => {
+                console.log(response)
+            })
         }
+
+        if (this.qty == 0) {
+
+        }
+
+
     }
+
 
 }
