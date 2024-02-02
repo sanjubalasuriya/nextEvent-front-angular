fa8ae3907b78a21480ab5b02b793b09fb79bfdea
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-02-02 13:46:36 +0530

    update main-slider.component

--- src/app/pages/home/home-page/inner-items/main-slider/main-slider.component.ts
+++ src/app/pages/home/home-page/inner-items/main-slider/main-slider.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {CarouselModule, OwlOptions} from "ngx-owl-carousel-o";
 
 @Component({
   selector: 'app-main-slider',
   standalone: true,
-  imports: [],
+    imports: [
+        CarouselModule
+    ],
   templateUrl: './main-slider.component.html',
   styleUrl: './main-slider.component.scss'
 })
 export class MainSliderComponent {
 
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 500,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 500,
+    navText: ['', ''],
+
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  }
 }
